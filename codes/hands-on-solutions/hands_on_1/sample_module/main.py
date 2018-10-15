68654354131f090b372130f3b725f1a9130ebcfa
Berg <berg@ice.ufjf.br>
2018-10-15 13:36:14 -0300

    Adding hands-on solutions

---
+++ codes/hands-on-solutions/hands_on_1/sample_module/main.py
@@ -0,0 +1,13 @@
+from parser import url_parse
+
+# Funcao principal em Python
+def main ():
+
+	# Chamar a funcao 'url_parse' do modulo parser
+	a = url_parse( "http://www.ufjf.br/deptocomputacao/sou-aluno/plano-departamental/" )
+	print a
+	
+if __name__ == "__main__":
+	main()	
+
+
---
+++ codes/hands-on-solutions/hands_on_2/solution_1.py
@@ -0,0 +1,32 @@
+# A funcao chamada pelo 'map' ira trabalhar em cada elemento do array
+def calcula_tamanho (s):
+	return len(s)
+
+def solucao1 (lista):
+	resp = []
+	n = len(lista)
+	for i in range(n):
+		resp.append(len(lista[i]))
+	return resp
+
+def solucao2 (lista):
+	return map(calcula_tamanho,lista)	
+
+# Funcao principal em Python
+def main ():
+	# Criando uma lista
+	lista = ["Lucas","Barbara","Thais","Bernardo","Ruy"]
+	print lista
+
+	r1 = solucao1(lista)
+	print r1
+
+	r2 = solucao2(lista)
+	print r2
+
+	# Usando programacao funcional com 'map'
+	#lista_summed = map(sum, lista1, lista2)
+	#print lista_summed
+	
+if __name__ == "__main__":
+	main()	
