1dc1135ea7da6070681a509982ad2371ad0308a7
Berg <berg@ice.ufjf.br>
2018-10-31 10:58:18 -0300

    Removing hands-on solution

--- codes/ufjf/hands-on-solutions/hands_on_1/sample_module/parser.py
+++
@@ -1,7 +0,0 @@
-# Funcao que resolve o problema
-def url_parse ( url ):
-	# Quebrar a url em tokens utilizando o caracter '/' como marcador
-	tokens = url.split('/')
-
-	# Retornar o indice 2 do array de tokens ...
-	return tokens[2]
